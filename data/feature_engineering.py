34fa861b9f8424cacfc71c17e7b982a7a57410a2
yizhougu425 <yizhougu@umich.edu>
2024-11-15 20:38:01 -0500

    Augment NOAA data with weather.gov data
    Fix data missingness in last 3 days of data (two days prior to today through today) by augmenting with data from weather.gov.

--- data/feature_engineering.py
+++ data/feature_engineering.py
@@ -1,7 +1,8 @@
 # import libraries
 import os
-import numpy as np
 import pandas as pd
+from datetime import datetime, timedelta
+
 
 # relative file path to the processed data
 noaa_data_path = os.path.join(os.path.dirname(__file__), 'raw_data/noaa/to_csv')
@@ -14,18 +15,29 @@ def feature_engineering_noaa_climate_data(file_path: str) -> pd.DataFrame:
     :param file_path: Path to the CSV file containing the climate data for a specific city.
     :returns: A pandas DataFrame with the feature-engineered climate data.
     """
-    # Load the dataset
-    df = pd.read_csv(file_path, parse_dates=['DATE'])
+    # Load the NOAA dataset
+    noaa_df = pd.read_csv(file_path, parse_dates=['DATE'])
+
+    # Filter out today and prior 2 days of data due to incompleteness
+    # We will augment it with the weather.gov dataset
+    noaa_df = noaa_df[noaa_df['DATE'] < (pd.Timestamp.now() - timedelta(days=4))]
 
     # Climate variables of interest
     climate_vars = ['PRCP', 'TMIN', 'TAVG', 'TMAX']
-    df = df[['DATE'] + climate_vars]
+    noaa_df = noaa_df[['DATE'] + climate_vars]
 
     # Convert temperature variables from tenths of a degree Celsius to Farenheit
     # (see NOAA docs: https://www.ncei.noaa.gov/pub/data/ghcn/daily/readme.txt)
-    df['TMIN'] = df['TMIN'] / 10 * (9 / 5) + 32
-    df['TAVG'] = df['TAVG'] / 10 * (9 / 5) + 32
-    df['TMAX'] = df['TMAX'] / 10 * (9 / 5) + 32
+    noaa_df['TMIN'] = noaa_df['TMIN'] / 10 * (9 / 5) + 32
+    noaa_df['TAVG'] = noaa_df['TAVG'] / 10 * (9 / 5) + 32
+    noaa_df['TMAX'] = noaa_df['TMAX'] / 10 * (9 / 5) + 32
+
+    # Load the weather.gov dataset, which is already measured in Farenheit
+    weather_gov_file_path = file_path.replace('noaa', 'weather_gov')
+    weather_gov_df = feature_engineering_weather_gov_data(weather_gov_file_path)
+
+    # Combine the two datasets
+    df = pd.concat([noaa_df, weather_gov_df], axis=0)
 
     # Add year, month columns for plotting
     df['YEAR'] = df['DATE'].dt.year
@@ -40,8 +52,8 @@ def feature_engineering_noaa_climate_data(file_path: str) -> pd.DataFrame:
     df['WEEK_OF_YEAR'] = df['DATE'].dt.isocalendar().week
 
     # Add a column for the season
-    df['SEASON'] = (df['DATE'].dt.month % 12 + 3) // 3
     # season_map = {1: 'Winter', 2: 'Spring', 3: 'Summer', 4: 'Fall'}
+    df['SEASON'] = (df['DATE'].dt.month % 12 + 3) // 3
 
     # Add columns for lagged versions of the climate variables
     for var in climate_vars:
@@ -59,7 +71,7 @@ def feature_engineering_noaa_climate_data(file_path: str) -> pd.DataFrame:
     for var in climate_vars:
         df[f'{var}_mean_5d_window'] = df[var].shift(365).rolling(window=5).mean()
 
-    # Convert all values in the df to float
+    # Drop date column and convert all values in the df to float
     df = df.drop('DATE', axis=1).astype(float)
 
     # Drop all observations with missing values
@@ -83,6 +95,37 @@ def feature_engineering_noaa_climate_data(file_path: str) -> pd.DataFrame:
     ] for item in sublist]]
 
     return df
+
+# function to perform feature engineering on each city's weather.gov climate dataset that was converted to .csv.
+def feature_engineering_weather_gov_data(file_path: str) -> pd.DataFrame:
+    """
+    :param file_path: Path to the CSV file containing the weather.gov climate data for a specific city.
+    :returns: A pandas DataFrame with the feature-engineered climate data.
+    """
+    # Load the dataset
+    df = pd.read_csv(file_path)
+
+    # Filter out today's calendar date due to data incompleteness
+    df = df[df['Date'] < pd.Timestamp.now().day]
+    df.rename(columns={'Date': 'DATE'}, inplace=True)
+
+    # Compute daily aggregated (daily min, max, avg) temperature values and aggregate precipitation
+    df_daily = df.groupby('DATE').agg(
+        PRCP=('Precipitation (in) 1 hr', 'sum'),
+        TMIN=('Temperature (ÂºF) Air', 'min'),
+        TAVG=('Temperature (ÂºF) Air', 'mean'),
+        TMAX=('Temperature (ÂºF) Air', 'max')
+    ).reset_index()
+
+    # Convert aggregate precipitation from in to tenths of mm
+    df_daily['PRCP'] = df_daily['PRCP'] * 254
+
+    # Convert date column to datetime based on stored value of day of month and current calendar date
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    df_daily['DATE'] = df_daily['DATE'].apply(lambda x: datetime(current_year, current_month, x))
+
+    return df_daily
 
 
 if __name__ == "__main__":
